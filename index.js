677a99a3755d8c383c35d8dccd91e8fb058c9a92
Abderrahmane Boudi <abderrahmane.boudi@mbis-inc.net>
2018-11-13 16:10:23 +0100

    Add the exclude option for keys that should not be snake-cased

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 var map = require('map-obj')
 var snakeCase = require('to-snake-case')
 
+const has = (array, key) => array.some(x => typeof x === 'string' ? x === key : x.test(key));
+
 module.exports = function (obj, options) {
   options = Object.assign({deep: true}, options)
+  const {exclude} = options;
 
   return map(obj, function (key, val) {
-    return [snakeCase(key), val]
+    if (!(exclude && has(exclude, key))) {
+      key = snakeCase(key);
+    }
+    return [key, val]
   }, options)
 }
