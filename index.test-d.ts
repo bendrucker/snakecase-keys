35d553cbb3d9a7b5d1a9c468875d7e6b2836f1f9
Todd Brown <mrbrownt@me.com>
2020-01-07 09:41:27 -0600

    Added interface type test

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,15 @@
 import { expectType } from 'tsd';
 import camelcaseKeys = require('.');
 
+interface TestInterface {
+  foo: boolean
+}
+
+const test: TestInterface = { foo: false }
+expectType<{ [key: string]: unknown }>(
+  camelcaseKeys(test)
+)
+
 expectType<Array<{ [key: string]: unknown }>>(
   camelcaseKeys([{ 'foo-bar': true }]),
 );
