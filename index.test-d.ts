72bf72c3e728dd08db14735ad7bb062cdfa354dd
Junya Watanabe <watanabe.junya.b612@gmail.com>
2021-10-01 11:48:49 +0900

    test: Fix test code and remove package.json.

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,18 +1,23 @@
-import { expectType } from "tsd";
+import { expectAssignable, expectType } from "tsd";
 import snakecaseKeys from ".";
 
 // 1. Test object conversion
 expectType<{ foo_bar: boolean }>(snakecaseKeys({ fooBar: true }));
-expectType<{ [key: string]: any }>(snakecaseKeys({ fooBar: true }));
+expectAssignable<{ [key: string]: boolean }>(snakecaseKeys({ fooBar: true }));
 
 // 2. Test array conversion
 expectType<{ foo_bar: boolean }[]>(snakecaseKeys([{ fooBar: true }]));
-expectType<Array<{ [key: string]: any }>>(snakecaseKeys([{ fooBar: true }]));
+expectAssignable<Array<{ [key: string]: boolean }>>(
+  snakecaseKeys([{ fooBar: true }])
+);
 expectType<string[]>(snakecaseKeys(["name 1", "name 2"]));
 
 // 3. Test deep conversion
-expectType<{ foo_bar: { "foo-bar": { "foo bar": boolean } } }>(
-  snakecaseKeys({ foo_bar: { "foo-bar": { "foo bar": true } } }, { deep: false })
+expectType<{ foo_bar: { "foo-bar": { "foo bar": true } } }>(
+  snakecaseKeys(
+    { foo_bar: { "foo-bar": { "foo bar": true } } },
+    { deep: false }
+  )
 );
 expectType<{ foo_bar: { foo_bar: { foo_bar: boolean } } }>(
   snakecaseKeys({ foo_bar: { "foo-bar": { "foo bar": true } } }, { deep: true })
@@ -24,7 +29,7 @@ expectType<{ foo_bar: { foo_bar: boolean } }[]>(
 // 4. Test exclusive conversion
 expectType<{ foo_bar: boolean; barBaz: true }>(
   snakecaseKeys(
-    { fooBar: true, barBaz: true, bazQux: true },
+    { fooBar: true, barBaz: true },
     {
       exclude: ["foo", "barBaz", /^baz/] as const,
     }
