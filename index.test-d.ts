5dc07b45152e21f936d667ef18bf116979264d39
Ben Drucker <bvdrucker@gmail.com>
2021-10-25 10:31:48 -0700

    trim wordy comments

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,18 +1,18 @@
 import { expectAssignable, expectType } from "tsd";
 import snakecaseKeys from ".";
 
-// 1. Test object conversion
+// Object
 expectType<{ foo_bar: boolean }>(snakecaseKeys({ fooBar: true }));
 expectAssignable<{ [key: string]: boolean }>(snakecaseKeys({ fooBar: true }));
 
-// 2. Test array conversion
+// Array
 expectType<{ foo_bar: boolean }[]>(snakecaseKeys([{ fooBar: true }]));
 expectAssignable<Array<{ [key: string]: boolean }>>(
   snakecaseKeys([{ fooBar: true }])
 );
 expectType<string[]>(snakecaseKeys(["name 1", "name 2"]));
 
-// 3. Test deep conversion
+// Deep
 expectType<{ foo_bar: { "foo-bar": { "foo bar": true } } }>(
   snakecaseKeys(
     { foo_bar: { "foo-bar": { "foo bar": true } } },
@@ -26,7 +26,7 @@ expectType<{ foo_bar: { foo_bar: boolean } }[]>(
   snakecaseKeys([{ "foo-bar": { foo_bar: true } }], { deep: true })
 );
 
-// 4. Test exclusive conversion
+// Exclude
 expectType<{ foo_bar: boolean; barBaz: true }>(
   snakecaseKeys(
     { fooBar: true, barBaz: true },
