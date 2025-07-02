ce4528044dace035e693c56e244782baf745dfca
wobsoriano <sorianorobertc@gmail.com>
2025-07-02 09:32:11 -0700

    chore: update jsdoc

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,23 @@ import map from 'map-obj';
 import { snakeCase, type Options as SnakeCaseOptions } from 'change-case';
 import type { SnakeCase } from 'type-fest';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
 type EmptyTuple = [];
 
+// Allow union with, for example, `undefined` and `null`.
 type ObjectUnion = Record<string, unknown> | unknown;
 
-// Allow fallback to any type for WithDefault
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// prettier-ignore
+/**
+ * Return a default type if input type is nil.
+ * @template T - Input type.
+ * @template U - Default type.
+*/
 type WithDefault<T, U> = T extends undefined | void | null ? U : T;
 
+/**
+ * Check if an element is included in a tuple.
+ * @template List - List of values.
+ * @template Target - Target to search.
+*/
 type Includes<List extends readonly unknown[], Target> = List extends undefined
   ? false
   : List extends Readonly<EmptyTuple>
@@ -22,10 +29,22 @@ type Includes<List extends readonly unknown[], Target> = List extends undefined
     : Includes<Rest, Target>
   : boolean;
 
+/**
+ * Append a segment to dot-notation path.
+ * @template S - Base path.
+ * @template Last - Additional path.
+*/
 type AppendPath<S extends string, Last extends string> = S extends ''
   ? Last
   : `${S}.${Last}`;
 
+/**
+ * Convert keys of an object to snake-case strings.
+ * @template T - Input object or array.
+ * @template Deep - Deep conversion flag.
+ * @template Exclude - Excluded keys.
+ * @template Path - Path of keys.
+*/
 export type SnakeCaseKeys<
   T extends ObjectUnion | ReadonlyArray<Record<string, unknown>>,
   Deep extends boolean = true,
@@ -49,28 +68,28 @@ export type SnakeCaseKeys<
 
 export interface Options {
   /**
-  Recurse nested objects and objects in arrays.
-  @default true
+   * Recurse nested objects and objects in arrays.
+   * @default true
   */
   readonly deep?: boolean;
   /**
-  Exclude keys from being snakeCased.
-  @default []
+   * Exclude keys from being snakeCased.
+   * @default []
   */
   readonly exclude?: ReadonlyArray<string | RegExp>;
   /**
-  A function that determines whether to recurse for a specific key and value.
+   * A function that determines whether to recurse for a specific key and value.
   */
   readonly shouldRecurse?: (key: any, value: any) => boolean;
   /**
-  Options object that gets passed to snake-case parsing function.
-  @default {}
+   * Options object that gets passed to snake-case parsing function.
+   * @default {}
   */
   readonly parsingOptions?: SnakeCaseOptions;
 }
 
 /**
- * Convert object keys to snake using [`snake-case`](https://github.com/blakeembrey/change-case/tree/master/packages/snake-case).
+ * Convert object keys to snake using [`snake-case`](https://github.com/blakeembrey/change-case/tree/main/packages/change-case).
  * @param input - Object or array of objects to snake-case.
  * @param options - Options of conversion.
  */
