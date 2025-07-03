f9f6cfd1c44a9ecd216eb8c3e1fcc178820e7029
Ben Drucker <bvdrucker@gmail.com>
2025-07-03 09:01:38 -0700

    accept custom snake case fn (#161)

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ module.exports = function (obj, options) {
 
   options = Object.assign({ deep: true, exclude: [], parsingOptions: {} }, options)
 
+  const convertCase = options.snakeCase || ((key) => snakeCase(key, options.parsingOptions))
+
   return map(obj, function (key, val) {
     return [
-      matches(options.exclude, key) ? key : snakeCase(key, options.parsingOptions),
+      matches(options.exclude, key) ? key : convertCase(key),
       val,
       mapperOptions(key, val, options)
     ]
