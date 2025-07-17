a76d1c732556b31494bf725f5bc753a84e5fb0ec
Ben Drucker <bvdrucker@gmail.com>
2025-07-17 16:06:27 -0700

    test: tape -> stdlib (#171)

--- test.js
+++ test.js
@@ -1,70 +1,63 @@
 'use strict'
 
-import test from 'tape'
+import { test } from 'node:test'
+import assert from 'node:assert'
 import Snake from './index.js'
 
-test('basic functionality', (t) => {
-  t.deepEqual(Snake({ fooBar: 'baz', nested: { fooBar: 'baz' } }), { foo_bar: 'baz', nested: { foo_bar: 'baz' } })
-  t.end()
+test('basic functionality', () => {
+  assert.deepEqual(Snake({ fooBar: 'baz', nested: { fooBar: 'baz' } }), { foo_bar: 'baz', nested: { foo_bar: 'baz' } })
 })
 
-test('repeated capital letters', (t) => {
-  t.deepEqual(Snake({ fooID: 1 }), { foo_id: 1 })
-  t.end()
+test('repeated capital letters', () => {
+  assert.deepEqual(Snake({ fooID: 1 }), { foo_id: 1 })
 })
 
-test('shallow conversion with {deep: false}', (t) => {
-  t.deepEqual(
+test('shallow conversion with {deep: false}', () => {
+  assert.deepEqual(
     Snake({ fooBar: { barBaz: 'qux' } }, { deep: false }),
     { foo_bar: { barBaz: 'qux' } }
   )
-  t.end()
 })
 
-test('array of objects', (t) => {
+test('array of objects', () => {
   const result = Snake([{ fooBar: 'baz' }])
-  t.deepEqual(result, [{ foo_bar: 'baz' }])
-  t.ok(Array.isArray(result))
-  t.end()
+  assert.deepEqual(result, [{ foo_bar: 'baz' }])
+  assert.ok(Array.isArray(result))
 })
 
-test('nested arrays', (t) => {
+test('nested arrays', () => {
   const result = Snake({ foo: [0, 1, 2] })
-  t.deepEqual(result, { foo: [0, 1, 2] })
-  t.ok(Array.isArray(result.foo))
-  t.end()
+  assert.deepEqual(result, { foo: [0, 1, 2] })
+  assert.ok(Array.isArray(result.foo))
 })
 
-test('snakecase objects in arrays', (t) => {
+test('snakecase objects in arrays', () => {
   const result = Snake({ foo: [0, { fooBar: 'baz', nested: { fooBar: 'baz' } }, 2] })
-  t.deepEqual(result, { foo: [0, { foo_bar: 'baz', nested: { foo_bar: 'baz' } }, 2] })
-  t.ok(Array.isArray(result.foo))
-  t.end()
+  assert.deepEqual(result, { foo: [0, { foo_bar: 'baz', nested: { foo_bar: 'baz' } }, 2] })
+  assert.ok(Array.isArray(result.foo))
 })
 
-test('exclude', (t) => {
-  t.deepEqual(
+test('exclude', () => {
+  assert.deepEqual(
     Snake({ fooBar: 'baz', barBaz: 'qux' }, { exclude: ['fooBar'] }),
     { fooBar: 'baz', bar_baz: 'qux' }
   )
-  t.deepEqual(
+  assert.deepEqual(
     Snake({ fooBar: 'baz', barBaz: 'qux' }, { exclude: [/^foo/, /^bar/] }),
     { fooBar: 'baz', barBaz: 'qux' }
   )
-  t.end()
 })
 
-test('parsing options', (t) => {
+test('parsing options', () => {
   const splitOnCamelCase = input => input.split(/(?=[A-Z])/)
-  t.deepEqual(
+  assert.deepEqual(
     Snake({ 'fooBar.baz': 'qux', 'bar.bazQux': 'foo' }, { parsingOptions: { split: splitOnCamelCase } }),
     { 'foo_bar.baz': 'qux', 'bar.baz_qux': 'foo' }
   )
-  t.end()
 })
 
-test('shouldRecurse option', (t) => {
-  t.deepEqual(
+test('shouldRecurse option', () => {
+  assert.deepEqual(
     Snake(
       { fooBar: { barBaz: 'qux' }, nested: { barBaz: 'qux' } },
       { deep: true, shouldRecurse: (key, val) => key !== 'nested' }
@@ -72,7 +65,7 @@ test('shouldRecurse option', (t) => {
     { foo_bar: { bar_baz: 'qux' }, nested: { barBaz: 'qux' } }
   )
   const date = new Date()
-  t.deepEqual(
+  assert.deepEqual(
     Snake(
       { fooBar: { barBaz: 'qux' }, fooDate: date },
       { deep: true, shouldRecurse: (key, val) => !(val instanceof Date) }
@@ -82,91 +75,74 @@ test('shouldRecurse option', (t) => {
       foo_date: date
     }
   )
-  t.end()
 })
 
-test('not a plain object(primitive value)', (t) => {
-  t.throws(
+test('not a plain object(primitive value)', () => {
+  assert.throws(
     () => Snake(1),
-    { message: 'obj must be an plain object' },
-    'Should throw an error when input is not a plain object'
+    { message: 'obj must be an plain object' }
   )
-  t.end()
 })
 
-test('not a plain object(function value)', (t) => {
-  t.throws(
+test('not a plain object(function value)', () => {
+  assert.throws(
     () => Snake(() => 1),
-    { message: 'obj must be an plain object' },
-    'Should throw an error when input is not a plain object'
+    { message: 'obj must be an plain object' }
   )
-  t.end()
 })
 
-test('not a plain object(instance value)', (t) => {
-  t.throws(
+test('not a plain object(instance value)', () => {
+  assert.throws(
     () => Snake(new Date()),
-    { message: 'obj must be an plain object' },
-    'Should throw an error when input is not a plain object'
+    { message: 'obj must be an plain object' }
   )
-  t.end()
 })
 
-test('not array of plain objects(primitive value)', (t) => {
-  t.throws(
+test('not array of plain objects(primitive value)', () => {
+  assert.throws(
     () => Snake([1, { fooBar: 'baz' }]),
-    { message: 'obj must be array of plain objects' },
-    'Should throw an error when input is not an array of plain objects'
+    { message: 'obj must be array of plain objects' }
   )
-  t.end()
 })
 
-test('not array of plain objects(function value)', (t) => {
-  t.throws(
+test('not array of plain objects(function value)', () => {
+  assert.throws(
     () => Snake([() => 1, { fooBar: 'baz' }]),
-    { message: 'obj must be array of plain objects' },
-    'Should throw an error when input is not an array of plain objects'
+    { message: 'obj must be array of plain objects' }
   )
-  t.end()
 })
 
-test('not array of plain objects(instance value)', (t) => {
-  t.throws(
+test('not array of plain objects(instance value)', () => {
+  assert.throws(
     () => Snake([new Date(), { fooBar: 'baz' }]),
-    { message: 'obj must be array of plain objects' },
-    'Should throw an error when input is not an array of plain objects'
+    { message: 'obj must be array of plain objects' }
   )
-  t.end()
 })
 
-test('custom snakeCase function', (t) => {
+test('custom snakeCase function', () => {
   const customSnakeCase = (key) => key.replace(/([A-Z])/g, '_$1').toLowerCase()
-  t.deepEqual(
+  assert.deepEqual(
     Snake({ fooBar: 'baz', barBaz: 'qux' }, { snakeCase: customSnakeCase }),
     { foo_bar: 'baz', bar_baz: 'qux' }
   )
-  t.end()
 })
 
-test('custom snakeCase function with nested objects', (t) => {
+test('custom snakeCase function with nested objects', () => {
   const customSnakeCase = (key) => key.toUpperCase()
-  t.deepEqual(
+  assert.deepEqual(
     Snake({ fooBar: { barBaz: 'qux' } }, { snakeCase: customSnakeCase }),
     { FOOBAR: { BARBAZ: 'qux' } }
   )
-  t.end()
 })
 
-test('undefined values in arrays with deep: true', (t) => {
-  t.deepEqual(
+test('undefined values in arrays with deep: true', () => {
+  assert.deepEqual(
     Snake({ fooBar: [undefined, 'value'] }, { deep: true }),
     { foo_bar: [undefined, 'value'] }
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     Snake({ nested: { fooBar: [undefined, { bazQux: 'value' }] } }, { deep: true }),
     { nested: { foo_bar: [undefined, { baz_qux: 'value' }] } }
   )
-
-  t.end()
 })
